1d20eaf81f605519a3842f29f788c70c3218476d
Gamila El-Fayoumi <117528749+Gamila-Elfayoumi@users.noreply.github.com>
2023-03-13 17:32:47 +0200

    Add files via upload

---
+++ Grade_calculation.py
@@ -0,0 +1,8 @@
+firstgrade = input (" Enter first grade out of 100 : ")
+secondgrade = input (" Enter second grade out of 100 : ")
+thirdgrade = input (" Enter third grade out of 100 : ")
+x = float (firstgrade)
+y = float (secondgrade)
+z = float (thirdgrade)
+final_grade = ((30*x)/100)+((30*y)/100)+((40*z)/100)
+print ("The final grade is equal to ", final_grade)
